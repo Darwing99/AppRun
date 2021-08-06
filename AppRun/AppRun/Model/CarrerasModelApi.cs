a766606913d6415d78b1cd67e59c386ea6efee82
Darwing <darwing01000100@gmail.com>
2021-08-05 23:59:44 -0600

    modelosCarreras

---
+++ AppRun/AppRun/Model/CarrerasModelApi.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppRun.Model
+{
+  
+    public class CarrerasModelApi
+    {
+        [JsonProperty("idToken")]
+        public int idToken { get; set; }
+        [JsonProperty("carrera")]
+        public object carrera { get; set; }
+        [JsonProperty("fecha")]
+        public object fecha { get; set; }
+        [JsonProperty("distancia")]
+        public int distancia { get; set; }
+        [JsonProperty("fotos")]
+        public int fotos { get; set; }
+        [JsonProperty("tiempo")]
+        public int tiempo { get; set; }
+        [JsonProperty("estado")]
+        public bool estado { get; set; }
+        [JsonProperty("idUser")]
+        public int idUser { get; set; }
+    }
+
+
+}
---
+++ AppRun/AppRun/services/RestApiCarreras.cs
@@ -0,0 +1,65 @@
+﻿using AppRun.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRun.services
+{
+    public class RestApiCarreras
+    {
+
+
+        HttpClient cliente;
+
+        public RestApiCarreras()
+        {
+            cliente = new HttpClient();
+        }
+        public async Task<List<CarrerasModelApi>> GetRepositoriesAsync(string url)
+        {
+            List<CarrerasModelApi> lista = null;
+            try
+            {
+                HttpResponseMessage respuesta = await cliente.GetAsync(url);
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    string informacion = await respuesta.Content.ReadAsStringAsync();
+                    lista = JsonConvert.DeserializeObject<List<CarrerasModelApi>>(informacion);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error", ex.Message);
+            }
+
+            return lista;
+        }
+
+
+
+        public async Task<List<CarrerasModelApi>> DeleteTodoItemAsync(string url)
+        {
+            List<CarrerasModelApi> lista = null;
+
+
+            HttpResponseMessage response = await cliente.DeleteAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+
+            }
+            return lista;
+        }
+
+
+     
+
+    }
+
+
+
+}
